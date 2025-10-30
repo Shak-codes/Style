cc632d95a2c306e580b82330070102383dda7229
Shakir <shakirrahman81@gmail.com>
2025-10-30 18:35:28 -0400

    Adds appointments page

---
+++ frontend/src/app/(auth)/appointments/[id]/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.scss";
+
+const NewClient = () => {
+  return (
+    <section className={styles.container}>
+      <h1>Add a new client</h1>
+    </section>
+  );
+};
+
+export default NewClient;
--- frontend/src/app/(auth)/appointments/page.tsx
+++ frontend/src/app/(auth)/appointments/page.tsx
@@ -1,5 +1,38 @@
+import { NavButton, Table } from "@/components/server";
+import { APPOINTMENTS } from "../tableData";
+import styles from "./styles.module.scss";
+
+const columns = [
+  { header: "Client", type: "text" },
+  { header: "Service", type: "text" },
+  { header: "Price", type: "text" },
+  { header: "Status", type: "status" },
+  { header: "Date", type: "date" },
+  { header: "Duration", type: "text" },
+  { header: "Prebook?", type: "bool" },
+] as const;
+
+const appointmentData = APPOINTMENTS.map((appointment) => [
+  appointment.client,
+  appointment.service.name,
+  appointment.service.cost,
+  appointment.status,
+  appointment.start,
+  appointment.length,
+  appointment.isPrebook,
+]);
+
 const Appointments = () => {
-  return <h1>Your Appointments</h1>;
+  return (
+    <section className={styles.container}>
+      <h1>Your Appointments</h1>
+      <p>See every past and future appointment</p>
+      <section className={styles.tableHeader}>
+        <NavButton href="/appointments/new" text="Add Appointment" />
+      </section>
+      <Table columns={columns} data={appointmentData} />
+    </section>
+  );
 };
 
 export default Appointments;
