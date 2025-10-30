cff356bacd37a627a58a7e694c5f997a69ca4fca
Shakir <shakirrahman81@gmail.com>
2025-10-30 18:51:44 -0400

    Adds filters to services

--- frontend/src/app/(auth)/services/page.tsx
+++ frontend/src/app/(auth)/services/page.tsx
@@ -1,7 +1,11 @@
-import Table from "@/components/server/Table/Table";
+"use client";
+
 import { SERVICES } from "../tableData";
 import styles from "./styles.module.scss";
-import { NavButton } from "@/components/server";
+import { NavButton, Table } from "@/components/server";
+import { Filter } from "@/components/client";
+import { FilterProps } from "@/components/client/Filter/types";
+import { useState } from "react";
 
 const columns = [
   { header: "Name", type: "text" },
@@ -16,14 +20,57 @@ const serviceData = SERVICES.map((service) => [
 ]);
 
 const Services = () => {
+  const [filters, setFilters] = useState<FilterProps>([
+    {
+      label: "Name",
+      property: "name",
+      type: "text",
+      value: "",
+      operator: null,
+      onChange: (operator, value) => updateValue(0, operator, value),
+    },
+    {
+      label: "Category",
+      property: "category",
+      type: "text",
+      value: "",
+      operator: null,
+      onChange: (operator, value) => updateValue(1, operator, value),
+    },
+    {
+      label: "Cost",
+      property: "cost",
+      type: "number",
+      value: null,
+      operator: null,
+      onChange: (operator, value) => updateValue(2, operator, value),
+    },
+  ]);
+
+  const updateValue = (
+    index: number,
+    operator: string | null,
+    value: string | number | number[] | null
+  ) => {
+    setFilters((prev) => {
+      const updatedFilters = [...prev];
+      updatedFilters[index] = {
+        ...updatedFilters[index],
+        operator,
+        value,
+      };
+      return updatedFilters;
+    });
+  };
   return (
     <section className={styles.container}>
       <h1>Your Services</h1>
       <p>Create, modify, and keep track of your current services.</p>
       <section className={styles.tableHeader}>
-        <NavButton href="/services/new" text="Add Service" />
+        <Filter filters={filters} />
       </section>
       <Table columns={columns} data={serviceData} />
+      <NavButton href="/services/new" text="Add Service" />
     </section>
   );
 };
